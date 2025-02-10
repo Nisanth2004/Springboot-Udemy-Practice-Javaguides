1b65aeb4aa4ced559f129cba9af3ba9770e5482d
Nisanth2004 <nisanthjothi2004@gmail.com>
2025-02-10 08:17:25 +0530

    Build getEmployeeById,getAllEmployees REST API

--- Employee-Management-System/ems-backend/src/main/java/com/nisanth/emsbackend/controller/EmployeeController.java
+++ Employee-Management-System/ems-backend/src/main/java/com/nisanth/emsbackend/controller/EmployeeController.java
@@ -1,14 +1,14 @@
 package com.nisanth.emsbackend.controller;
 
 import com.nisanth.emsbackend.dto.EmployeeDto;
+import com.nisanth.emsbackend.entity.Employee;
 import com.nisanth.emsbackend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -26,4 +26,20 @@ public class EmployeeController {
         return new ResponseEntity<>(savedEmploye, HttpStatus.CREATED);
     }
 
+    // Build Get Empoloyee By id REST API
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable Long id)
+    {
+        EmployeeDto employeeDto=employeeService.getEmployeeById(id);
+        return new ResponseEntity<>(employeeDto,HttpStatus.OK);
+    }
+
+
+    // Build Get All  Empoloyees By id REST API
+    @GetMapping
+    public ResponseEntity<List<EmployeeDto>> getALlEmployees()
+    {
+       List<EmployeeDto> employees= employeeService.getAllEmployees();
+       return ResponseEntity.ok(employees);
+    }
 }
--- Employee-Management-System/ems-backend/src/main/java/com/nisanth/emsbackend/service/impl/EmployeeServiceImpl.java
+++ Employee-Management-System/ems-backend/src/main/java/com/nisanth/emsbackend/service/impl/EmployeeServiceImpl.java
@@ -3,10 +3,14 @@ package com.nisanth.emsbackend.service.impl;
 import com.nisanth.emsbackend.dto.EmployeeDto;
 import com.nisanth.emsbackend.dto.EmployeeMapper;
 import com.nisanth.emsbackend.entity.Employee;
+import com.nisanth.emsbackend.exception.ResourceNotFoundException;
 import com.nisanth.emsbackend.repository.EmployeeRepository;
 import com.nisanth.emsbackend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -24,5 +28,24 @@ public class EmployeeServiceImpl implements EmployeeService {
         // return the employeeDto instead of entity to aviod to transfer sensitive
 
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+
+      Employee employee=  employeeRepository.findById(employeeId)
+                .orElseThrow(()->new ResourceNotFoundException("Employee does not exist by this id: "+employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+
+       List<Employee> employees= employeeRepository.findAll();
+
+       // employee entity is convert to list of employee DTO's
+
+        return employees.stream().map((employee )->EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
     }
 }
